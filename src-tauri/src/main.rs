4a0a56e2c5f7ef4f76223a19a2da39fb1d0ff22a
SutekhVRC <sutekhvrc@gmail.com>
2022-11-07 20:54:05 -0800

    - Added system tray functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 )]
 
 use parking_lot::Mutex;
-use tauri::Manager;
+use tauri::{Manager, SystemTrayMenu};
 
 mod config;
 mod vcupdate;
@@ -17,7 +17,40 @@ mod lovense;
 
 fn main() {
 
+    let quit = tauri::CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide_app = tauri::CustomMenuItem::new("hide".to_string(), "Hide");
+    let show_app = tauri::CustomMenuItem::new("show".to_string(), "Show");
+
+    let tray_menu = SystemTrayMenu::new()
+    .add_item(hide_app)
+    .add_item(show_app)
+    .add_native_item(tauri::SystemTrayMenuItem::Separator)
+    .add_item(quit);
+
     let app = tauri::Builder::default()
+    .system_tray(tauri::SystemTray::new().with_menu(tray_menu))
+    .on_system_tray_event(|app, event| match event {
+        tauri::SystemTrayEvent::MenuItemClick { id, .. } => {
+            match id.as_str() {
+                "quit" => {
+                    app.exit(0);
+                },
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                },
+                "show" => {
+                    app.windows().iter().for_each(|w| {
+                        println!("{}", w.0);
+                    });
+                    let window = app.get_window("main").unwrap();
+                    window.show().unwrap();
+                }
+                _ => {},
+            }
+        },
+        _ => {}
+    })
     .manage(
         vcore::VCStateMutex(
             Mutex::new(
@@ -38,10 +71,20 @@ fn main() {
     .expect("Failed to generate Tauri context");
 
     app.run(|_app_handle, event| match event {
+        tauri::RunEvent::WindowEvent { label, event, .. } => {
+            match event {
+                tauri::WindowEvent::CloseRequested { api, .. } => {
+                    let window = _app_handle.get_window(&label).unwrap();
+                    window.hide().unwrap();
+                    api.prevent_close();
+                }, _ => {}
+            }
+        },
         tauri::RunEvent::ExitRequested { .. } => {
-            println!("Exit Request!");
+            // On exit
         },
         tauri::RunEvent::MainEventsCleared => {
+
             let state = _app_handle.state::<vcore::VCStateMutex>();
             let vc_lock = state.0.lock();
             
