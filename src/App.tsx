7323e8de496f432b9870e8dc4f67aa06c53eae8f
TuTiDore <TuTiDore@gmail.com>
2022-12-21 20:12:28 -0500

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { ToysProvider } from "./context/ToysContext";
 import Toys from "./features/Toys";
 import { CoreEventProvider } from "./context/CoreEventContext";
 import { Footer } from "./features/Footer";
+import VibecheckLogo from "./assets/VibeCheck_logo.png";
 import "./App.css";
 
 export default function App() {
   return (
     <CoreEventProvider>
-      <h1>
-        <div className="grad-forewards text-clip text-8xl">VibeCheck</div>
-      </h1>
+      <div className="flex justify-center h-20 mb-4">
+        <img src={VibecheckLogo} />
+      </div>
       <ToysProvider>
         <Toys />
       </ToysProvider>
