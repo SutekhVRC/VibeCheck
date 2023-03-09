c381f70ce2e90419aa35a9ec11268b60c0a3cc56
TuTiDore <TuTiDore@gmail.com>
2023-03-08 22:49:51 -0500

    update config files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: true,
   theme: {
     extend: {},
   },
