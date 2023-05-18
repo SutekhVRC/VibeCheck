ed2a96066e7cffac8897c285957ce079cdb0d781
TuTiDore <tutidore@gmail.com>
2023-05-18 18:10:28 -0400

    add some type annotations

--- src/hooks/useVersion.tsx
+++ src/hooks/useVersion.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 export function useVersion() {
   const [version, setVersion] = useState("");
   async function getVersion() {
-    await invoke<string>(VERSION).then((v) => setVersion(v));
+    await invoke<string>(VERSION).then((v:string) => setVersion(v));
   }
   useEffect(() => {
     getVersion();
