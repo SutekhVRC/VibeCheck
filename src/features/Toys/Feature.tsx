6980bf1619482f4e9558e73e6bd9b9ad1ea3681a
TuTiDore <tutidore@gmail.com>
2023-05-18 19:03:38 -0400

    add feature animations

--- src/features/Toys/Feature.tsx
+++ src/features/Toys/Feature.tsx
@@ -1,7 +1,9 @@
 import type { FeVCToyFeature } from "../../../src-tauri/bindings/FeVCToyFeature";
 import FeatureForm from "./FeatureForm";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
-import { Disclosure } from "@headlessui/react";
+import classNames from "classnames";
+import { AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 export function Feature({
   toyId,
@@ -10,33 +12,29 @@ export function Feature({
   toyId: number;
   feature: FeVCToyFeature;
 }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Disclosure>
-      {({ open }) => (
-        <>
-          <Disclosure.Button>
-            <div className="flex justify-between items-center pl-2 pr-2">
-              <div className="flex items-center">
-                <div
-                  className={
-                    feature.feature_enabled ? "text-gray-200" : "text-gray-500"
-                  }
-                >
-                  {feature.feature_type} {feature.feature_index}
-                </div>
-              </div>
-              <ChevronUpIcon
-                className={`${
-                  open && "rotate-180 transform"
-                } h-6 w-6 text-zinc-400`}
-              />
-            </div>
-          </Disclosure.Button>
-          <Disclosure.Panel>
-            <FeatureForm toyId={toyId} toyFeature={feature} />
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+    <>
+      <button className="flex justify-between items-center pl-2 pr-2" onClick={() => setIsOpen(b => !b)}>
+        <div className="flex items-center">
+          <div
+            className={
+              feature.feature_enabled ? "text-gray-200" : "text-gray-500"
+            }
+          >
+            {feature.feature_type} {feature.feature_index}
+          </div>
+        </div>
+        <ChevronUpIcon
+          className={classNames(
+            isOpen && "rotate-180 transform",
+            "h-6 w-6 text-zinc-400"
+          )}
+        />
+      </button>
+      <AnimatePresence>
+        {isOpen && <FeatureForm toyId={toyId} toyFeature={feature} />}
+      </AnimatePresence>
+    </>
   );
 }
--- src/features/Toys/FeatureForm.tsx
+++ src/features/Toys/FeatureForm.tsx
@@ -12,6 +12,7 @@ import type { FeVCToyFeature } from "../../../src-tauri/bindings/FeVCToyFeature"
 import Slider from "../../layout/Slider";
 import TooltipLabel from "../../layout/Tooltip/TooltipLabel";
 import useSimulate from "../../hooks/useSimulate";
+import { motion } from "framer-motion";
 
 type ToyFeatureFormProps = {
   toyId: number;
@@ -77,121 +78,133 @@ export default function FeatureForm({
     useSimulate(toyId, toyFeature.feature_index, toyFeature.feature_type);
 
   return (
-    <div className="grid grid-cols-[minmax(6rem,_1fr)_1fr_minmax(6rem,_3fr)_1fr] text-sm text-justify gap-y-1 p-4">
-      <TooltipLabel text="Enabled" tooltip="Enable/Disable this feature." />
-      <input
-        type="checkbox"
-        name="feature_enabled"
-        checked={feature.feature_enabled}
-        onChange={handleFeatureCheckbox}
-      />
-      <div></div>
-      <div></div>
-      <TooltipLabel
-        text="OSC Parameter"
-        tooltip="The float OSC parameter to control this feature's motor."
-      />
-      <div></div>
-      <input
-        className="text-zinc-800 text-xs"
-        name="osc_parameter"
-        value={feature.osc_parameter.replace(OSC_PARAM_PREFIX, "")}
-        onChange={handleOscParam}
-      />
-      <div></div>
-      <TooltipLabel
-        text="Flip Input"
-        tooltip="Some toys use a flipped float input. Enable this if your toy seems to do the opposite motor level you were expecting."
-      />
-      <input
-        type="checkbox"
-        name="flip_input_float"
-        checked={feature.flip_input_float}
-        onChange={handleFeatureCheckbox}
-      />
-      <div></div>
-      <div></div>
-      <TooltipLabel
-        text="Smoothing"
-        tooltip="This smooths the float input by queueing the amount set with the slider, then transforming them into one value to send instead. If you aren't sending a lot of floats rapidly over OSC you probably want this disabled completely."
-      />
-      <input
-        type="checkbox"
-        name="smooth_enabled"
-        checked={feature.smooth_enabled}
-        onChange={handleFeatureCheckbox}
-      />
-      <Slider
-        disabled={!feature.smooth_enabled}
-        min={1}
-        max={20}
-        step={1}
-        value={[levels.smooth_rate]}
-        onValueChange={(e) => handleLevels("smooth_rate", e[0])}
-      />
-      <div className="text-right">{levels.smooth_rate}</div>
-      <TooltipLabel
-        text="Idle"
-        tooltip="Set the idle motor speed for this feature. Idle activates when there is no input. Your set idle speed won't activate until you send at least one float value in the valid min/max range you have set."
-      />
-      <div></div>
-      <Slider
-        min={0}
-        max={1}
-        step={0.01}
-        value={[levels.idle_level]}
-        onValueChange={(e) => handleLevels("idle_level", e[0])}
-      />
-      <div className="text-right">{round0.format(levels.idle_level * 100)}</div>
-      <TooltipLabel
-        text="Minimum"
-        tooltip="The minimum motor speed that will be sent to the feature's motor."
-      />
-      <div></div>
-      <Slider
-        min={0}
-        max={1}
-        step={0.01}
-        value={[levels.minimum_level]}
-        onValueChange={(e) => handleLevels("minimum_level", e[0])}
-      />
-      <div className="text-right">
-        {round0.format(levels.minimum_level * 100)}
+    <motion.div
+      initial={{ height: 0, opacity: 0 }}
+      exit={{ height: 0, opacity: 0 }}
+      animate={{
+        height: "auto",
+        opacity: 1,
+        transition: { type: "spring", bounce: 0.3, duration: 0.4 },
+      }}
+    >
+      <div className="grid grid-cols-[minmax(6rem,_1fr)_1fr_minmax(6rem,_3fr)_1fr] text-sm text-justify gap-y-1 p-4">
+        <TooltipLabel text="Enabled" tooltip="Enable/Disable this feature." />
+        <input
+          type="checkbox"
+          name="feature_enabled"
+          checked={feature.feature_enabled}
+          onChange={handleFeatureCheckbox}
+        />
+        <div></div>
+        <div></div>
+        <TooltipLabel
+          text="OSC Parameter"
+          tooltip="The float OSC parameter to control this feature's motor."
+        />
+        <div></div>
+        <input
+          className="text-zinc-800 text-xs"
+          name="osc_parameter"
+          value={feature.osc_parameter.replace(OSC_PARAM_PREFIX, "")}
+          onChange={handleOscParam}
+        />
+        <div></div>
+        <TooltipLabel
+          text="Flip Input"
+          tooltip="Some toys use a flipped float input. Enable this if your toy seems to do the opposite motor level you were expecting."
+        />
+        <input
+          type="checkbox"
+          name="flip_input_float"
+          checked={feature.flip_input_float}
+          onChange={handleFeatureCheckbox}
+        />
+        <div></div>
+        <div></div>
+        <TooltipLabel
+          text="Smoothing"
+          tooltip="This smooths the float input by queueing the amount set with the slider, then transforming them into one value to send instead. If you aren't sending a lot of floats rapidly over OSC you probably want this disabled completely."
+        />
+        <input
+          type="checkbox"
+          name="smooth_enabled"
+          checked={feature.smooth_enabled}
+          onChange={handleFeatureCheckbox}
+        />
+        <Slider
+          disabled={!feature.smooth_enabled}
+          min={1}
+          max={20}
+          step={1}
+          value={[levels.smooth_rate]}
+          onValueChange={(e) => handleLevels("smooth_rate", e[0])}
+        />
+        <div className="text-right">{levels.smooth_rate}</div>
+        <TooltipLabel
+          text="Idle"
+          tooltip="Set the idle motor speed for this feature. Idle activates when there is no input. Your set idle speed won't activate until you send at least one float value in the valid min/max range you have set."
+        />
+        <div></div>
+        <Slider
+          min={0}
+          max={1}
+          step={0.01}
+          value={[levels.idle_level]}
+          onValueChange={(e) => handleLevels("idle_level", e[0])}
+        />
+        <div className="text-right">
+          {round0.format(levels.idle_level * 100)}
+        </div>
+        <TooltipLabel
+          text="Minimum"
+          tooltip="The minimum motor speed that will be sent to the feature's motor."
+        />
+        <div></div>
+        <Slider
+          min={0}
+          max={1}
+          step={0.01}
+          value={[levels.minimum_level]}
+          onValueChange={(e) => handleLevels("minimum_level", e[0])}
+        />
+        <div className="text-right">
+          {round0.format(levels.minimum_level * 100)}
+        </div>
+        <TooltipLabel
+          text="Maximum"
+          tooltip="The maximum motor speed that will be sent to the feature's motor."
+        />
+        <div></div>
+        <Slider
+          min={0}
+          max={1}
+          step={0.01}
+          value={[levels.maximum_level]}
+          onValueChange={(e) => handleLevels("maximum_level", e[0])}
+        />
+        <div className="text-right">
+          {round0.format(levels.maximum_level * 100)}
+        </div>
+        <div className="h-2" />
+        <div />
+        <div />
+        <div />
+        <TooltipLabel text="Simulate" tooltip="Test feature power level." />
+        <input
+          type="checkbox"
+          name="simulate"
+          checked={simulate}
+          onChange={simulateHandler}
+        />
+        <Slider
+          min={0}
+          max={1}
+          step={0.01}
+          value={[simulateLevel]}
+          onValueChange={(e) => simulateLevelHandler(e[0])}
+        />
+        <div className="text-right">{round0.format(simulateLevel * 100)}</div>
       </div>
-      <TooltipLabel
-        text="Maximum"
-        tooltip="The maximum motor speed that will be sent to the feature's motor."
-      />
-      <div></div>
-      <Slider
-        min={0}
-        max={1}
-        step={0.01}
-        value={[levels.maximum_level]}
-        onValueChange={(e) => handleLevels("maximum_level", e[0])}
-      />
-      <div className="text-right">
-        {round0.format(levels.maximum_level * 100)}
-      </div>
-      <div className="h-2" />
-      <div />
-      <div />
-      <div />
-      <TooltipLabel text="Simulate" tooltip="Test feature power level." />
-      <input
-        type="checkbox"
-        name="simulate"
-        checked={simulate}
-        onChange={simulateHandler}
-      />
-      <Slider
-        min={0}
-        max={1}
-        step={0.01}
-        value={[simulateLevel]}
-        onValueChange={(e) => simulateLevelHandler(e[0])}
-      />
-      <div className="text-right">{round0.format(simulateLevel * 100)}</div>
-    </div>
+    </motion.div>
   );
 }
