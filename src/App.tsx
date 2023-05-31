51e10630be549cd1300ca99759c3eb3d39460648
TuTiDore <tutidore@gmail.com>
2023-05-30 20:55:01 -0400

    two panel layout, config main panel

--- src/App.tsx
+++ src/App.tsx
@@ -8,54 +8,79 @@ import Button from "./layout/Button";
 import { useCoreEventContext } from "./context/CoreEventContext";
 import "./App.css";
 import Loading from "./components/Loading";
+import { useState } from "react";
+import { FeVCToy } from "../src-tauri/bindings/FeVCToy";
+import classNames from "classnames";
 
 export default function App() {
+  const [selectedToy, setSelectedToy] = useState<FeVCToy | null>(null);
   const { onlineToys, offlineToys } = useToys();
-  const toyCount =
-    Object.values(onlineToys).length + Object.values(offlineToys).length;
+  const toys: FeVCToy[] = [];
+  Object.values(onlineToys).forEach((t) => toys.push(t));
+  Object.values(offlineToys).forEach((t) => toys.push(t));
   const { isScanning, toggleScan } = useCoreEventContext();
 
   return (
     <div className="w-screen h-screen p-4">
-      <div className="flex justify-center pb-2">
-        <img className="h-20 object-contain" src={VibecheckLogo} />
-      </div>
-      <div className="h-[calc(100vh-160px)] flex justify-center">
-        <div className="flex flex-col justify-between gap-2 text-lg rounded-lg p-3 bg-zinc-800 flex-grow max-w-xl">
-          {toyCount == 0 ? (
-            <div className="flex-grow flex justify-center">
-              <div className="flex flex-col justify-center items-center -mt-20">
-                <img src={cryingAnimeGirl} />
-                <div>No Toys</div>
-              </div>
-            </div>
-          ) : (
-            <div className="overflow-y-scroll pl-2 scrollbar">
-              <AnimatePresence>
-                {Object.values(onlineToys).map((toy) => (
-                  <Toy toy={toy} key={`${toy.toy_name} ${toy.sub_id}`} />
-                ))}
-                {Object.values(offlineToys).map((toy) => (
-                  <Toy toy={toy} key={`${toy.toy_name}`} />
-                ))}
-              </AnimatePresence>
-            </div>
-          )}
-          <div>
-            <Button onClick={toggleScan}>
-              {isScanning ? (
-                <div className="flex justify-center">
-                  <div>Scanning</div>
-                  <Loading />
+      <div className="grid grid-cols-[1fr,_4fr] h-[calc(100%-40px)] gap-3">
+        <div className="flex flex-col gap-2">
+          <img className="h-16 object-contain" src={VibecheckLogo} />
+          <div className=" bg-gray-800 rounded-md justify-between flex flex-col max-h-fit flex-grow">
+            {toys.length === 0 ? (
+              <div className="flex-grow flex justify-center">
+                <div className="flex flex-col justify-center items-center -mt-20">
+                  <img src={cryingAnimeGirl} />
+                  <div>No Toys</div>
                 </div>
-              ) : (
-                <div>Search for toys</div>
-              )}
-            </Button>
+              </div>
+            ) : (
+              <div className="flex flex-col overflow-y-scroll pl-2 scrollbar whitespace-nowrap">
+                <AnimatePresence>
+                  {Object.values(toys).map((toy) => (
+                    <button
+                      key={`${toy.toy_name} ${toy.sub_id}`}
+                      onClick={() => {
+                        setSelectedToy(toy);
+                      }}
+                      className={classNames(
+                        toy.toy_name == selectedToy?.toy_name &&
+                          toy.sub_id == selectedToy?.sub_id &&
+                          "outline",
+                        "bg-gray-700 rounded-md p-2 m-2 hover:bg-cyan-600 outline-2 outline-emerald-400"
+                      )}
+                    >
+                      {toy.toy_name}
+                    </button>
+                  ))}
+                </AnimatePresence>
+              </div>
+            )}
+            <div>
+              <Button onClick={toggleScan}>
+                {isScanning ? (
+                  <div className="flex justify-center">
+                    <div>Scanning</div>
+                    <Loading />
+                  </div>
+                ) : (
+                  <div>Search for toys</div>
+                )}
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-800 rounded-lg">
+          <div className="flex justify-between p-4">
+            {selectedToy != null && (
+              <Toy
+                toy={selectedToy}
+                key={`${selectedToy.toy_name} ${selectedToy.sub_id}`}
+              />
+            )}
           </div>
         </div>
       </div>
-      <div className="h-14 flex justify-center items-center">
+      <div className="m-2">
         <Footer />
       </div>
     </div>
