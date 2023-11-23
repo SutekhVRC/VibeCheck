59a320db0cec07a7485354cbf0fd3883d2d2cf33
SutekhVRC <sutekhvrc@gmail.com>
2023-11-23 05:34:50 -0800

    Unsure about frontend/backend conversion traits

--- src-tauri/src/frontend/mod.rs
+++ src-tauri/src/frontend/mod.rs
@@ -1,3 +1,13 @@
+//use ts_rs::TS;
+
 pub mod frontend_native;
 pub mod frontend_types;
 pub mod error;
+
+/* Trait for frontend type conversions? maybe
+pub trait VCFrontend<F, B> 
+where F: TS, B: TS,
+{
+    fn to_frontend(&self, backend_type: B) -> F;
+    fn to_backend(&self, frontend_type: F) -> B;
+}*/
