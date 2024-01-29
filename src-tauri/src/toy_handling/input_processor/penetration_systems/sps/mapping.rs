0318699041a6198defc9ba75e3f2817314a7df05
TuTiDore <tutidore@gmail.com>
2024-01-28 21:24:05 -0500

    simplify some logic

--- src-tauri/src/toy_handling/input_processor/penetration_systems/sps/mapping.rs
+++ src-tauri/src/toy_handling/input_processor/penetration_systems/sps/mapping.rs
@@ -64,25 +64,13 @@ impl SPSMapping {
 
     fn get_root_tip_osc_values(&self, others: bool) -> Option<(f64, f64)> {
         let values: (f64, f64) = if others {
-            let Some(root_value) = self.osc_values.get("PenOthersNewRoot") else {
-                return None;
-            };
-
-            let Some(tip_value) = self.osc_values.get("PenOthersNewTip") else {
-                return None;
-            };
-
-            (*root_value, *tip_value)
+            let root_value = *self.osc_values.get("PenOthersNewRoot")?;
+            let tip_value = *self.osc_values.get("PenOthersNewTip")?;
+            (root_value, tip_value)
         } else {
-            let Some(root_value) = self.osc_values.get("PenSelfNewRoot") else {
-                return None;
-            };
-
-            let Some(tip_value) = self.osc_values.get("PenSelfNewTip") else {
-                return None;
-            };
-
-            (*root_value, *tip_value)
+            let root_value = *self.osc_values.get("PenSelfNewRoot")?;
+            let tip_value = *self.osc_values.get("PenSelfNewTip")?;
+            (root_value, tip_value)
         };
 
         Some(values)
@@ -94,55 +82,35 @@ impl SPSMapping {
 
     pub fn update_mapping_length_values(&mut self, others: bool) {
         // Logic needs to be optimized (polymorphism/whatever)
+        let Some((root_value, tip_value)) = self.get_root_tip_osc_values(others) else {
+            return;
+        };
 
-        if others {
-            let Some((root_value, tip_value)) = self.get_root_tip_osc_values(others) else {
-                return;
-            };
-
-            if root_value < 0.01 || tip_value < 0.01 {
+        if root_value < 0.01 || tip_value < 0.01 {
+            if others {
                 self.length_values_others.clear();
-            }
-
-            if root_value > 0.95 {
-                return;
-            }
-
-            let temp_length = tip_value - root_value;
-
-            if temp_length < 0.02 {
-                return;
-            }
-
-            if tip_value > 0.99 {
-                // Use last value
-                // Should we have a bad length ?
             } else {
-                self.length_values_others.insert(0, temp_length);
-                self.length_values_others.truncate(SAVED_LENGTH_VALUES_MAX);
-            }
-        } else {
-            let Some((root_value, tip_value)) = self.get_root_tip_osc_values(others) else {
-                return;
-            };
-
-            if root_value < 0.01 || tip_value < 0.01 {
                 self.length_values_self.clear();
             }
+        }
 
-            if root_value > 0.95 {
-                return;
-            }
+        if root_value > 0.95 {
+            return;
+        }
 
-            let temp_length = tip_value - root_value;
+        let temp_length = tip_value - root_value;
 
-            if temp_length < 0.02 {
-                return;
-            }
+        if temp_length < 0.02 {
+            return;
+        }
 
-            if tip_value > 0.99 {
-                // Use last value
-                // Should we have a bad length ?
+        if tip_value > 0.99 {
+            // Use last value
+            // Should we have a bad length ?
+        } else {
+            if others {
+                self.length_values_others.insert(0, temp_length);
+                self.length_values_others.truncate(SAVED_LENGTH_VALUES_MAX);
             } else {
                 self.length_values_self.insert(0, temp_length);
                 self.length_values_self.truncate(SAVED_LENGTH_VALUES_MAX);
@@ -151,37 +119,30 @@ impl SPSMapping {
     }
 
     pub fn update_mapping_length(&mut self, others: bool) {
-        if others {
-            // Enough stored length values?
-            if self.length_values_others.len() < SAVED_LENGTH_VALUES_MIN {
-                // Just don't update length?
-                return;
-            }
+        let values_len = if others {
+            self.length_values_others.len()
         } else {
-            // Enough stored length values?
-            if self.length_values_self.len() < SAVED_LENGTH_VALUES_MIN {
-                // Just don't update length?
-                return;
-            }
+            self.length_values_self.len()
+        };
+        // Enough stored length values?
+        if values_len < SAVED_LENGTH_VALUES_MIN {
+            // Just don't update length?
+            return;
         }
     }
 
     pub fn update_level(&mut self, others: bool) -> Option<f64> {
         let (root_value, tip_value) = self.get_root_tip_osc_values(others)?;
-        if others {
-            if self.others_stored_length > 0. && tip_value > 0.99 {
-                let active_length = 1. - root_value;
-                let active_ratio = active_length / self.others_stored_length;
-                let level = 1. - active_ratio;
-                return Some(level);
-            }
+        let stored_length = if others {
+            self.others_stored_length
         } else {
-            if self.self_stored_length > 0. && tip_value > 0.99 {
-                let active_length = 1. - root_value;
-                let active_ratio = active_length / self.self_stored_length;
-                let level = 1. - active_ratio;
-                return Some(level);
-            }
+            self.self_stored_length
+        };
+        if stored_length > 0. && tip_value > 0.99 {
+            let active_length = 1. - root_value;
+            let active_ratio = active_length / stored_length;
+            let level = 1. - active_ratio;
+            return Some(level);
         }
 
         None
