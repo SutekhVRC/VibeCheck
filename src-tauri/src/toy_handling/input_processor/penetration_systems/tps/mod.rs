4c268aad630cbe517ebcd7c5752f7126854600ea
SutekhVRC <sutekhvrc@gmail.com>
2024-01-31 16:37:28 -0800

    Basic TPS processing

--- src-tauri/src/toy_handling/input_processor/penetration_systems/tps/mod.rs
+++ src-tauri/src/toy_handling/input_processor/penetration_systems/tps/mod.rs
@@ -10,10 +10,16 @@ pub struct TPSProcessor {
 
 impl InputProcessor for TPSProcessor {
     fn is_parameter(&self, param: &String) -> bool {
-        param.starts_with("/avatar/parameters/TPS_Internal")
+        param.starts_with("/avatar/parameters/TPS_Internal/")
     }
 
-    fn process(&mut self, _addr: &str, _input: ModeProcessorInputType) -> Option<f64> {
-        todo!()
+    fn process(&mut self, addr: &str, input: ModeProcessorInputType) -> Option<f64> {
+        let tps_param = addr.strip_prefix("/avatar/parameters/")?;
+
+        if tps_param.ends_with("Depth_In") | tps_param.ends_with("RootRoot") {
+            input.try_float()
+        } else {
+            None
+        }
     }
 }
